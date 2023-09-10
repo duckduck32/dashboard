807ba369d5d4585c717569b900dc21cabf224950
WINDOWWSSS <reynard.wijaya004@binus.ac.id>
2023-09-10 15:57:33 +0700

    add data application

---
+++ add_application.php
@@ -0,0 +1,73 @@
+<?php
+require('sidebar.php');
+?>
+
+<div class="content pb-0">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header"> <strong>ADD APPLICATION VULNERABIILITY</strong> <small></small>
+                <form action="" method="post">
+                    <div class="card-body card-block">
+                        <div class="form-group">
+                            <tr>
+                                <label for="vulnerability" class="form-control-label">Vulnerability</label>
+                                <td><input type="text" name="vulnerability" class="form-control" placeholder="Vulnerability" required></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <label for="severity" class="form-control-label">Severity</label>
+                                <select id="severity" name="severity" class="form-control">
+                                    <option value="Critical">Critical</option>
+                                    <option value="High">High</option>
+                                    <option value="Medium">Medium</option>
+                                    <option value="Low">Low</option>
+                                </select>
+                            </tr>
+                        </div>    
+                        <div class="form-group">
+                            <tr>
+                                <label for="count" class=" form-control-label">Date Found</label>
+                                <td><input type="date" name="date_found" class="form-control" id="" required></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <td></td>
+                                <td><input type="submit" name="proses" class="form-control" id=""></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <td></td>
+                                <td><button class="form-control"><a href="application.php">Back</a></button></td>
+                            </tr>
+                        </div>
+                            
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            
+<?php
+
+if (isset($_POST['proses'])) {
+    mysqli_query($connection, "insert into app_vulns set
+    status = 'Open',
+    vulnerability = '$_POST[vulnerability]',
+    severity = '$_POST[severity]',
+    date_found = '$_POST[date_found]',
+    date_remediated = '0000-00-00'");
+
+
+    echo "Data telah berhasil ditambahkan";
+    echo "<meta http-equiv=refresh content=1;URL='application.php'>";
+}
+
+?>
