c7475151799ec94aa19e087beea868552e4eadec
WINDOWWSSS <reynard.wijaya004@binus.ac.id>
2023-09-24 12:26:02 +0700

    PDF Table Done, add Library
    fpdf and fpdf sector for pie chart

--- reporting_infra.php
+++ reporting_infra.php
@@ -13,23 +13,53 @@ class PDF extends FPDF{
     }
 }
 
+
 $pdf = new FPDF('p', 'mm', 'A4');
 $pdf->addPage();
-$pdf->SetFont('Arial','B',6);
+$pdf->SetFont('Arial','B',14);
 
-$pdf->Cell(5,5,'ID',1,0);
-$pdf->Cell(10,5,'Status',1,0);
-$pdf->Cell(15,5,'Plugin ID',1,0,'C');
-$pdf->Cell(32,5,'Vulnerability',1,0);
-$pdf->Cell(12,5,'Severity',1,0);
-$pdf->Cell(22,5,'Hostname',1,0);
-$pdf->Cell(22,5,'IP',1,0);
-$pdf->Cell(10,5,'Count',1,0);
-$pdf->Cell(22,5,'Date Found',1,0);
-$pdf->Cell(22,5,'Date Remediated',1,0);
-$pdf->Cell(15,5,'Assigned To',1,0);
-$pdf->Cell(10,5,'Status',1,1);
+$queryCritical = "SELECT SUM(count) AS total_count FROM infra_vulns WHERE severity = 'Critical'";
+$resultCritical = $connection->query($queryCritical);
+if ($resultCritical){
+    $row = $resultCritical->fetch_assoc();
+    $totalCritical= $row['total_count'];
+}
 
+$queryHigh = "SELECT SUM(count) AS total_count FROM infra_vulns WHERE severity = 'High'";
+$resultHigh = $connection->query($queryHigh);
+if ($resultCritical){
+    $row = $resultHigh->fetch_assoc();
+    $totalHigh= $row['total_count'];
+}
+
+$queryMedium = "SELECT SUM(count) AS total_count FROM infra_vulns WHERE severity = 'Medium'";
+$resultMedium = $connection->query($queryMedium);
+if ($resultMedium){
+    $row = $resultMedium->fetch_assoc();
+    $totalMedium= $row['total_count'];
+}
+
+$queryLow = "SELECT SUM(count) AS total_count FROM infra_vulns WHERE severity = 'Low'";
+$resultLow = $connection->query($queryLow);
+if ($resultLow){
+    $row = $resultLow->fetch_assoc();
+    $totalLow= $row['total_count'];
+}
+
+
+$pdf->setLeftMargin(75);
+$pdf->Cell(35,5,'Severity',1,0,'C');
+$pdf->Cell(30,5,'Count',1,1,'C');
+$pdf->Cell(35,5,'Critical',1,0,'C');
+$pdf->Cell(30,5,$totalCritical,1,1,'C');
+$pdf->Cell(35,5,'High',1,0,'C');
+$pdf->Cell(30,5,$totalHigh,1,1,'C');
+$pdf->Cell(35,5,'Medium',1,0,'C');
+$pdf->Cell(30,5,$totalMedium,1,1,'C');
+$pdf->Cell(35,5,'Low',1,0,'C');
+$pdf->Cell(30,5,$totalLow,1,1,'C');
+
+/*
 $query=mysqli_query($connection,"select * from infra_vulns");
 while($data=mysqli_fetch_array($query)){
     $pdf->SetFont('Arial','',6);
@@ -52,7 +82,7 @@ while($data=mysqli_fetch_array($query)){
     $pdf->Cell(22,5,$data['date_remediated'],1,0);
     $pdf->Cell(15,5,$data['assigned_to'],1,0);
     $pdf->Cell(10,5,$data['status'],1,1);
-}
+*/
 
 $pdf->Output();
 ?>
